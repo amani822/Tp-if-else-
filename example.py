7509e6cf1a6a0439ac0681654224329a5c0b358d
amani822 <bradaidorsaf046@gmail.com>
2025-08-28 21:12:43 +0100

    Add files via upload

---
+++ example.py
@@ -0,0 +1,7 @@
+firstName=input("enter your first name: ")
+lastName=input("enter your last name: ")
+if firstName!=" " and lastName!=" " :
+  print("welcome",firstName ,lastName)
+else: 
+  print ("Error: both first name and last name must be provided")
+
